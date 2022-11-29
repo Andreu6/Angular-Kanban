2902554de277f6be85075cc7856e19b85578de16
Andreu6 <suarezbellavistaandreu@gmail.com>
2022-11-29 23:07:21 +0100

    TS

--- Nueva carpeta/src/app/tarea/tarea.component.ts
+++ Nueva carpeta/src/app/tarea/tarea.component.ts
@@ -1,5 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Tarea } from '../models/tarea-model';
+import {
+  Component,
+  Input,
+  OnInit
+} from '@angular/core';
+import {
+  Tarea
+} from '../models/tarea-model';
 
 @Component({
   selector: 'app-tarea',
@@ -10,29 +16,31 @@ export class TareaComponent implements OnInit {
 
   @Input() tareas: Tarea;
 
-  constructor() { 
+  constructor() {
     this.tareas = {};
   }
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+
+  fechas(fechaFin: Date) {
+
+    let fechaActual = new Date;
+    let fecha = new Date(fechaFin)
+    let fechaNaranja = new Date(fechaActual.setDate(fechaActual.getDate() + 1))
+
+    if (fechaNaranja.getUTCDate() == fecha.getUTCDate() && fechaNaranja.getUTCFullYear() == fecha.getUTCFullYear() &&
+      fecha.getUTCMonth() == fechaNaranja.getUTCMonth() && this.tareas.lista != "Finalizadas") {
+      return "naranja"
+
+    } else if (fechaActual > fecha && this.tareas.lista != "Finalizadas") {
+      return "rojo"
+
+    } else if (fechaActual > fecha && this.tareas.lista == "Finalizadas") {
+      return "verde"
+
+    } else {
+      return "gris"
+    }
   }
-
-fecha(tareas:any){
-
-}
-
-fechaColores(fechaFin: Date){
-
-  let fechaActual = new Date;
-  let fecha = new Date(fechaFin) 
-  let fechaNaranja = new Date(fechaActual.setDate(fechaActual.getDate() + 1))
-
-    if(fechaNaranja.getUTCDate() == fecha.getUTCDate() && fechaNaranja.getUTCFullYear() == fecha.getUTCFullYear() && fecha.getUTCMonth() == fechaNaranja.getUTCMonth() && this.tarea.lista !=  "Finalizadas"){return "naranja"
-   
-    } else if(fechaActual > fecha && this.tareas.lista !=  "Finalizadas"){return "rojo"
-    
-    } else if (fechaActual > fecha && this.tareas.lista ==  "Finalizadas"){ return "verde"
-    
-    } else { return "gris" }
-}
 
 }
