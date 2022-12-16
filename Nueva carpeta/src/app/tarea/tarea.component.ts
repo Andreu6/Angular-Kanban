21b114a4102135156c7148c683d9ddafcfcbd8b2
Andreu6 <suarezbellavistaandreu@gmail.com>
2022-12-16 22:53:24 +0100

    Colores fechas

--- Nueva carpeta/src/app/tarea/tarea.component.ts
+++ Nueva carpeta/src/app/tarea/tarea.component.ts
@@ -1,4 +1,7 @@
 import {
+  DatePipe
+} from '@angular/common';
+import {
   Component,
   Input,
   OnInit
@@ -17,30 +20,52 @@ export class TareaComponent implements OnInit {
   @Input() tareas: Tarea;
 
   constructor() {
-    this.tareas = {id:0, lista:"", img:"", titulo:"", usuarios:[{email:"", img:"", nick:"", alt:""}], fechaFin:new Date()};
+    this.tareas = {
+      id: 0,
+      lista: "",
+      img: "",
+      titulo: "",
+      usuarios: [{
+        email: "",
+        img: "",
+        nick: "",
+        alt: ""
+      }],
+      fechaFin: new Date()
+    };
   }
   ngOnInit(): void {}
-
 
   fechas(fechaFin: Date) {
 
     let fechaA = new Date;
     let fecha = new Date(fechaFin)
-    let fechaO = new Date(fechaA.setDate(fechaA.getDate() + 1))
+    let fechaO = new Date;
 
-    if (fechaO.getUTCDate() == fecha.getUTCDate() && fechaO.getUTCFullYear() == fecha.getUTCFullYear() &&
-      fecha.getUTCMonth() == fechaO.getUTCMonth() && this.tareas.lista != "Finalizadas") {
+    let fechaOrange = new Date(fechaA.setDate(fechaA.getDate() + 1))
+
+    if ((fechaO.getUTCDate() == fecha.getUTCDate() && fechaO.getUTCFullYear() == fecha.getUTCFullYear() && fecha.getUTCMonth() == fechaO.getUTCMonth() ||
+        (fechaO.getUTCDate() == fecha.getUTCDate() && fechaO.getUTCFullYear() == fecha.getUTCFullYear() && fecha.getUTCMonth() == fechaO.getUTCMonth())) &&
+      this.tareas.lista != "Finalizadas") {
       return "orange"
 
-    } else if (fechaA > fecha && this.tareas.lista == "Finalizadas") {
+    } else if (fechaA >
+      fecha && this.tareas.lista == "Finalizadas") {
       return "green"
 
-    } else if (fechaA > fecha && this.tareas.lista != "Finalizadas") {
+    } else if (fechaA >
+      fecha && this.tareas.lista != "Finalizadas") {
       return "red"
 
     } else {
       return "grey"
     }
+  }
+
+
+  DateString(fechaFin: Date) {
+    const datePipe = new DatePipe('en-US');
+    return datePipe.transform(fechaFin, 'yyyy-MM-dd');
   }
 
 }
